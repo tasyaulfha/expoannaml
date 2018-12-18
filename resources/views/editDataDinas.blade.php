383cf42c5810f0baefcac1a57a142bb634f1fcf9
tasyaulfha <39845613+tasyaulfha@users.noreply.github.com>
2018-12-19 04:07:14 +0700

    Update editDataDinas.blade.php

--- resources/views/editDataDinas.blade.php
+++ resources/views/editDataDinas.blade.php
@@ -8,6 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <title>LACAK - Lapor Cari Perbaiki</title>
+  <link rel="icon" type="image/png" href="/img/maps2.png">
   <!-- Bootstrap core CSS-->
   <link href="/vendor/bootstrap3/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom fonts for this template-->
@@ -48,7 +49,7 @@
               {{ csrf_field()}}
                <div class="form-group">
                 <label>Nama Pelapor:</label>
-                <input type="text" name="nama_infrastruktur" class="form-control" value="{{$laporan->user_id}}" readonly="">
+                <input type="text" name="nama_infrastruktur" class="form-control" value="{{$laporan->user->nama}}" readonly="">
               </div>
                <div class="form-group">
                 <label>Nama Infrastruktur:</label>
@@ -99,23 +100,7 @@
       <i class="fa fa-angle-up"></i>
     </a>
     <!-- Logout Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="/index">Logout</a>
-          </div>
-        </div>
-      </div>
-    </div>
+    @include('logout')
 
 
     <!-- Jquery JS-->
