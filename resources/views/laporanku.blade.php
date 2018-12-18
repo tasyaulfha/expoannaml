546d40d6033bbf1aaaaf2efaf62d03d9f3557493
tasyaulfha <39845613+tasyaulfha@users.noreply.github.com>
2018-12-19 04:18:22 +0700

    Update laporanku.blade.php

--- resources/views/laporanku.blade.php
+++ resources/views/laporanku.blade.php
@@ -10,20 +10,20 @@
   <title>LACAK - Lapor Cari Perbaiki</title>
   <link rel="icon" type="image/png" href="/img/maps2.png">
   <!-- Bootstrap core CSS-->
-  <link href="vendor/bootstrap3/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/bootstrap3/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom fonts for this template-->
-  <link href="vendor/font-awesome2/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="/vendor/font-awesome2/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <!-- Page level plugin CSS-->
-  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <link href="/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin.css" rel="stylesheet">
+  <link href="/css/sb-admin.css" rel="stylesheet">
 
 
    <!-- Font special for pages-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 
     <!-- Main CSS-->
-    <link href="css/main2.css" rel="stylesheet" media="all">
+    <link href="/css/main2.css" rel="stylesheet" media="all">
  
 </head>
 
@@ -36,48 +36,54 @@
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
-          <a href="#">Lihat Laporanku</a>
+          <a href="#">Laporkan</a>
         </li>
-        <li class="breadcrumb-item active">Laporanku</li>
+        <li class="breadcrumb-item active">Formulir Pelaporan</li>
       </ol>
       <div class="card mb-3">
         <div class="card-header">
-          <i class="fa fa-forms"></i>Laporanku</div>
+          <i class="fa fa-forms"></i>Formulir Pelaporan</div>
         <div class="card-body">
             <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-              <thead>
-                <tr>
-                  <th>No</th>
-                  <th>Nama Pelapor</th>
-                  <th>Nama Infrastruktur</th>
-                  <th>Jenis Kerusakan</th>
-                  <th>Tingkat Kerusakan</th>
-                  <th>Deskripsi</th>
-                  <th>Lokasi</th>
-                  <th>Dokumentasi</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php $no = 0;?>
-                  @foreach($lapor as $datas)
-                <?php $no++ ;?>
-                <tr>
-                  <td>{{$no}}</td>
-                  <td>{{$datas -> user -> nama}}</td>
-                  <td>{{$datas->nama_infrastruktur}}</td>
-                  <td>{{$datas->jenis_kerusakan}}</td>
-                  <td>{{$datas->tingkat_kerusakan}}</td>
-                  <td>{{$datas->deskripsi}}</td>
-                  <td>{{$datas->lokasi}}</td>
-                  <td>{{$datas->file}}</td>
-                  <td>{{$datas->status}}</td>
-                @endforeach
-              </tbody>
-            </table>
+            <form action="{{route('laporan.store')}}" method="POST" enctype="multipart/form-data">
+              {{ csrf_field()}}
+               <div class="form-group">
+                <label>Nama Infrastruktur:</label>
+                <input type="text" name="nama_infrastruktur" class="form-control" placeholder="Nama Infrastruktur">
               </div>
+               <div class="form-group">
+                <label>Jenis Kerusakan:</label>
+                <input type="text" name="jenis_kerusakan" class="form-control" placeholder="Jenis Kerusakan">
+              </div>
+               <div class="form-group">
+                <label>Tingkat Kerusakan:</label>
+                <select class="form-control" name="tingkat_kerusakan">
+                  <option value="">Select</option>
+                    @foreach($data as $d)
+                    <option value="{{$d->level}}">
+                      {{$d->level}}
+                    </option>
+                    @endforeach
+                </select>
+              </div>
+               <div class="form-group">
+                <label>Deskripsi</label>
+                <input type="text" name="deskripsi" class="form-control" placeholder="Deskripsi">
+              </div>
+              <div class="form-group">
+                <label>Lokasi</label>
+                <input type="text" name="lokasi" class="form-control" placeholder="Lokasi">
+              </div>
+              <div class="form-group">
+                <label>Unggah File</label>
+                <input type="file" name="file" id="file" class="">
+              </div>
+              <hr>
+              <br>
+              <form class="form-group">
+                <button class="btn btn--radius-2 btn--blue-2">Kirim</button>
+              </form>
+            </form>
             </div>
         </div>
       </div>
